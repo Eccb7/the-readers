fcb0f664a58bf87bc8380f03842045fd4388eb39
Briton Otieno <ojwangbriton@gmail.com>
2024-02-27 23:18:26 +0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get 'ratings', to: 'ratings#index'
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "libraries#index"
 end
