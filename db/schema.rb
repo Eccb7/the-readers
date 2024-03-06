5d0e58c636d47d55264034879c253d38304b5e85
Briton Otieno <ojwangbriton@gmail.com>
2024-03-06 21:43:42 +0300

    Add schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_170448) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_154938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_170448) do
   create_table "libraries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "score"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
