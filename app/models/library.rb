5b687d823eb16eb3c303a7c439cc488d544cb36e
Briton Otieno <ojwangbriton@gmail.com>
2024-03-07 22:47:51 +0300

    Update the library model

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,17 @@
 class Library < ApplicationRecord
   has_many :books
+
+  def year
+    read_attribute(:year)
+  end
+
+  def year=(value)
+    write_attribute(:year, value)
+  end
+
+  def print_report
+    puts name
+    puts "established at year #{year}"
+    puts "number of beers #{beers.count}"
+  end
 end
